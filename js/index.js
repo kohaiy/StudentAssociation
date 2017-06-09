11bdca5837f14899eb615ad1f0b1dfb0f8673c87
GQxQD <916309979@qq.com>
2017-06-09 08:51:56 +0800

    add
    2017年6月9日 08:51:51

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('#navbar-lock').click(function() {
+        if ($(this).attr('data') == 1) {
+        	$('#navbar').removeClass('navbar-fixed-top');
+        	$('body').css('margin-top','0');
+            $(this).attr('data', 0);
+            $(this).css('color', '#777');
+        } else {
+        	$('#navbar').addClass('navbar-fixed-top');
+        	$('body').css('margin-top','50px');
+            $(this).attr('data', 1);
+            $(this).css('color', 'green');
+        }
+    });
+});
