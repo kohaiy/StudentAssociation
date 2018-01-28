8642932aba1e2b3002fa329544bbac712be71820
gqxqd <916309979@qq.com>
2018-01-28 16:34:48 +0800

    delete the old files.

--- runtime/temp/b411583e9f9cd04a55ef578ba3db13ca.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:74:"E:\git\StudentAssociation\public/../application/index\view\board\test.html";i:1499084195;}*/ ?>
-<div>
-<ul>
-<?php if(is_array($list) || $list instanceof \think\Collection || $list instanceof \think\Paginator): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$post): $mod = ($i % 2 );++$i;?>
-    <li> <?php echo $post['pid']; ?></li>
-<?php endforeach; endif; else: echo "" ;endif; ?>
-</ul>
-</div>
-<?php echo $list->render(); ?>
